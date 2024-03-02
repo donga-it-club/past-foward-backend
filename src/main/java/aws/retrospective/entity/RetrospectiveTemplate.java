98d549533f90b971350b0591b5f7c177c61365e5
정민석 <ms.jung@fanmaum.com>
2024-03-02 23:24:49 +0900

    feat: RetrospectiveTemplate 테이블을 추가합니다.

---
+++ src/main/java/aws/retrospective/entity/RetrospectiveTemplate.java
@@ -0,0 +1,27 @@
+package aws.retrospective.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RetrospectiveTemplate extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name; // ex) KPT, KWAT
+
+    @Builder
+    public RetrospectiveTemplate(String name) {
+        this.name = name;
+    }
+}
