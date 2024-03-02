5122e6f3cb50035ee83e0fc6c8f1e08965a41b06
정민석 <ms.jung@fanmaum.com>
2024-03-02 23:31:06 +0900

    feat: RetrospectiveTemplate의 name size를 지정합니다.

--- src/main/java/aws/retrospective/entity/RetrospectiveTemplate.java
+++ src/main/java/aws/retrospective/entity/RetrospectiveTemplate.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,6 +19,7 @@ public class RetrospectiveTemplate extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(max = 10)
     private String name; // ex) KPT, KWAT
 
     @Builder
