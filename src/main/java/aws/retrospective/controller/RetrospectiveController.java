97b1153ef0e39f90806572e81de0db7dc609e987
정민석 <ms.jung@fanmaum.com>
2024-03-03 22:01:27 +0900

    feat: add createRetrospective endpoint

---
+++ src/main/java/aws/retrospective/controller/RetrospectiveController.java
@@ -0,0 +1,27 @@
+package aws.retrospective.controller;
+
+
+import aws.retrospective.dto.CreateRetrospectiveDto;
+import aws.retrospective.dto.CreateRetrospectiveResponseDto;
+import aws.retrospective.service.RetrospectiveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/retrospectives")
+public class RetrospectiveController {
+    private final RetrospectiveService retrospectiveService;
+
+    @Autowired
+    public RetrospectiveController(RetrospectiveService retrospectiveService) {
+        this.retrospectiveService = retrospectiveService;
+    }
+
+    @PostMapping()
+    public CreateRetrospectiveResponseDto createRetrospective(@RequestBody CreateRetrospectiveDto createRetrospectiveDto) {
+        return retrospectiveService.createRetrospective(createRetrospectiveDto);
+    }
+}
