f98c23ceb6ac26b156473a428e39fd8129d5bc09
정민석 <ms.jung@fanmaum.com>
2024-03-04 21:37:41 +0900

    refactor: 불필요한 repository 어노테이션을 제거합니다.

--- src/main/java/aws/retrospective/repository/RetrospectiveRepository.java
+++ src/main/java/aws/retrospective/repository/RetrospectiveRepository.java
@@ -3,9 +3,7 @@ package aws.retrospective.repository;
 
 import aws.retrospective.entity.Retrospective;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RetrospectiveRepository extends JpaRepository<Retrospective, Long> {
 
 }
--- src/main/java/aws/retrospective/repository/UserRepository.java
+++ src/main/java/aws/retrospective/repository/UserRepository.java
@@ -3,9 +3,7 @@ package aws.retrospective.repository;
 
 import aws.retrospective.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
