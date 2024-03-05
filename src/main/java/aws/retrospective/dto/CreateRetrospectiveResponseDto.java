8649cfba1405d08d5183e4d51784882b80fff2e1
정민석 <ms.jung@fanmaum.com>
2024-03-05 21:43:59 +0900

    fix: 불필요한 어노테이션 제거

--- src/main/java/aws/retrospective/dto/CreateRetrospectiveResponseDto.java
+++ src/main/java/aws/retrospective/dto/CreateRetrospectiveResponseDto.java
@@ -1,12 +1,10 @@
 package aws.retrospective.dto;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
+@Builder
 public class CreateRetrospectiveResponseDto {
 
     private Long id;
