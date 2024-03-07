f598549b33044560e2fe22f8804ab2049356542d
정민석 <ms.jung@fanmaum.com>
2024-03-07 21:28:26 +0900

    feat: API 문서화 추가

--- src/main/java/aws/retrospective/controller/RetrospectiveTemplateController.java
+++ src/main/java/aws/retrospective/controller/RetrospectiveTemplateController.java
@@ -3,6 +3,8 @@ package aws.retrospective.controller;
 
 import aws.retrospective.dto.RetrospectiveTemplateResponseDto;
 import aws.retrospective.service.RetrospectiveTemplateService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/retrospective-template")
+@RequestMapping("/retrospective-templates")
+@Tag(name = "retrospective-templates")
 public class RetrospectiveTemplateController {
 
     private final RetrospectiveTemplateService retrospectiveTemplateService;
 
-
+    @Operation(summary = "회고 템플릿 리스트 조회")
     @GetMapping()
     public List<RetrospectiveTemplateResponseDto> getRetrospectiveTemplates() {
         return retrospectiveTemplateService.getRetrospectiveTemplates();
