fd1fce953c7e16a142067a23406e4b94f8db3c97
llcodingll <ll.o.llwhkr@gmail.com>
2024-03-08 22:15:44 +0900

    Create CommentRepository.java

---
+++ src/main/java/aws/retrospective/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package aws.retrospective.repository;
+
+import aws.retrospective.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+
+}
