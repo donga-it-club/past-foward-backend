51635e6ebf9bf5e456a588a6df413892e9ba0528
정민석 <ms.jung@fanmaum.com>
2024-03-09 22:25:13 +0900

    feat: GetRetrospectivesDto 추가

---
+++ src/main/java/aws/retrospective/dto/GetRetrospectivesDto.java
@@ -0,0 +1,27 @@
+package aws.retrospective.dto;
+
+
+import aws.retrospective.entity.ProjectStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class GetRetrospectivesDto {
+
+    int page = 0;
+
+    int size = 10;
+
+    RetrospectivesOrderType order = RetrospectivesOrderType.RECENTLY;
+
+    ProjectStatus status;
+
+    String keyword;
+
+
+}
