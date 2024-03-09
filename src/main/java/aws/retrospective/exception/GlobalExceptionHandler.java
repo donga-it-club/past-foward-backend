130f134625fd70973a58abd9bb9dbaf88c5b6594
w1shope <wishhopelee@gmail.com>
2024-03-09 21:32:09 +0900

    feat: 공통 응답 API 추가

---
+++ src/main/java/aws/retrospective/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package aws.retrospective.exception;
+
+import aws.retrospective.common.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        log.error("handleException", ex);
+        ErrorResponse response = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse> receiveRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.errorResponse(HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
+}
