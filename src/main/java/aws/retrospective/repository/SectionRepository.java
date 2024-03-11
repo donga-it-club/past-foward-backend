558c83f365a178d4ec8ef66b62ab07b86ecd8fe6
w1shope <wishhopelee@gmail.com>
2024-03-11 16:10:23 +0900

    chore: 불필요한 로직 제거

--- src/main/java/aws/retrospective/repository/SectionRepository.java
+++ src/main/java/aws/retrospective/repository/SectionRepository.java
@@ -2,7 +2,6 @@ package aws.retrospective.repository;
 
 import aws.retrospective.entity.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,8 +9,4 @@ public interface SectionRepository extends JpaRepository<Section, Long> {
 
     @Query(value = "SELECT COUNT(s.templateSection.sectionName) FROM Section s")
     int findSectionSequence(@Param("sectionName") String sectionName);
-
-    @Modifying
-    @Query(value = "UPDATE Section s SET s.content = :updateContent where s.id = :sectionId")
-    void updateSectionContent(@Param("updateContent") String content, @Param("sectionId") Long id);
 }
