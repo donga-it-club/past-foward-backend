f6b0bec5138559215a541aab7c4eeba4fd9c27da
llcodingll <ll.o.llwhkr@gmail.com>
2024-03-11 21:34:33 +0900

    Update CommentController.java

--- src/main/java/aws/retrospective/controller/CommentController.java
+++ src/main/java/aws/retrospective/controller/CommentController.java
@@ -2,10 +2,16 @@ package aws.retrospective.controller;
 
 import aws.retrospective.entity.Comment;
 import aws.retrospective.service.CommentService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/comments")
 public class CommentController {
 
     private final CommentService commentService;
@@ -15,5 +21,35 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-    
+    @GetMapping
+    public ResponseEntity<List<Comment>> getAllComments() {
+        List<Comment> comments = commentService.getAllComments();
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Comment> getCommentById(@PathVariable Long id) {
+        Optional<Comment> comment = commentService.getCommentId(id);
+        return comment.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+            .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Comment> createComment(@RequestBody Comment comment) {
+        Comment createdComment = commentService.createComment(comment);
+        return new ResponseEntity<>(createdComment, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Comment> updateComment(@PathVariable Long id, @RequestBody Comment updatedComment) {
+        Comment updated = commentService.updateComment(id, updatedComment);
+        return updated != null ? new ResponseEntity<>(updated, HttpStatus.OK)
+            : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long id) {
+        commentService.deleteComment(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
