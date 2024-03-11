79205fbcca4a613bb6f7f996e730c387cd13ee59
w1shope <wishhopelee@gmail.com>
2024-03-11 16:06:08 +0900

    fix: 검증 애너테이션 추가

--- src/main/java/aws/retrospective/controller/SectionController.java
+++ src/main/java/aws/retrospective/controller/SectionController.java
@@ -5,11 +5,11 @@ import aws.retrospective.dto.CreateSectionResponseDto;
 import aws.retrospective.dto.EditSectionRequestDto;
 import aws.retrospective.dto.EditSectionResponseDto;
 import aws.retrospective.service.SectionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +29,7 @@ public class SectionController {
 
     // 특정 섹션 수정
     @PatchMapping("/{sectionId}")
-    public EditSectionResponseDto editSectionContent(@PathVariable Long sectionId, @RequestBody EditSectionRequestDto request) {
+    public EditSectionResponseDto editSectionContent(@PathVariable Long sectionId, @Valid @RequestBody EditSectionRequestDto request) {
         return sectionService.updateSectionContent(sectionId, request);
     }
 }
