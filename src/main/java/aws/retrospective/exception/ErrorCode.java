3b41f0a46e5363bb1c794ce114f04774eb188373
w1shope <wishhopelee@gmail.com>
2024-03-12 23:09:35 +0900

    feat: 유효성 검사 예외 핸들러 추가

--- src/main/java/aws/retrospective/exception/ErrorCode.java
+++ src/main/java/aws/retrospective/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ErrorCode {
     BAD_REQUEST(400, "BAD_REQUEST_400"),
     NOT_FOUND(404, "NOT_FOUND_404"),
-    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR_500");
+    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR_500"),
+    MISSING_REQUEST_PARAMETER(400, "MISSING REQUEST PARAMETER");
 
     private int code;
     private String message;
