9588101853ffa2da12cfdf41a1ca9d83be2d5b7e
w1shope <wishhopelee@gmail.com>
2024-03-17 22:35:21 +0900

    chore: DB column명 수정

--- src/main/java/aws/retrospective/entity/Likes.java
+++ src/main/java/aws/retrospective/entity/Likes.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -23,9 +24,11 @@ public class Likes extends BaseEntity{
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "section_id")
     private Section section;
 
     @Builder
