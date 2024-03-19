a31d9c356504ed72d6954df5c0401c8c4fb603f6
zzero-o <2216132@donga.ac.kr>
2024-03-19 15:11:16 +0900

    Create SurveyRepository.java

---
+++ src/main/java/aws/retrospective/repository/SurveyRepository.java
@@ -0,0 +1,10 @@
+package aws.retrospective.repository;
+
+import aws.retrospective.entity.Survey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
+
+}
