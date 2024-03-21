154c93fb4b48c5eaef7825add64d91c10c1e24c6
llcodingll <ll.o.llwhkr@gmail.com>
2024-03-22 02:26:03 +0900

    feat: SurveyService 추가

---
+++ src/main/java/aws/retrospective/service/SurveyService.java
@@ -0,0 +1,36 @@
+package aws.retrospective.service;
+
+import aws.retrospective.dto.SurveyDto;
+import aws.retrospective.entity.Survey;
+import aws.retrospective.repository.SurveyRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SurveyService {
+
+    private final SurveyRepository surveyRepository;
+
+    public List<SurveyDto> getAllSurveys() {
+        List<Survey> surveys = surveyRepository.findAll();
+
+        return surveys.stream()
+            .map(this::convertToDto)
+            .collect(Collectors.toList());
+    }
+
+    private SurveyDto convertToDto(Survey survey) {
+        return SurveyDto.builder()
+            .id(survey.getId())
+            .age(survey.getAge())
+            .gender(survey.getGender())
+            .occupation(survey.getOccupation())
+            .region(survey.getRegion())
+            .source(survey.getSource())
+            .purpose(survey.getPurpose())
+            .build();
+    }
+}
