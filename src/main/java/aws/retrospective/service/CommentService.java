79f086c782ae57654891719ae0cf6f65cfe496e6
llcodingll <ll.o.llwhkr@gmail.com>
2024-03-22 01:16:21 +0900

    fix: CommentService 수정

--- src/main/java/aws/retrospective/service/CommentService.java
+++ src/main/java/aws/retrospective/service/CommentService.java
@@ -4,6 +4,9 @@ import aws.retrospective.dto.CommentDto;
 import aws.retrospective.entity.Comment;
 import aws.retrospective.repository.CommentRepository;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,11 +22,42 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
+    public List<CommentDto> getAllComments() {
+        List<Comment> comments = commentRepository.findAll();
+        return comments.stream()
+            .map(this::convertToDTO)
+            .collect(Collectors.toList());
+    }
     public CommentDto getCommentDTOById(Long id) {
         Comment comment = commentRepository.findById(id)
             .orElseThrow(() -> new EntityNotFoundException("Comment not found"));
 
         return convertToDTO(comment);
+    }
+
+    public Optional<Comment> getCommentById(Long id) {
+        return commentRepository.findById(id);
+    }
+
+    public Comment createComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public Comment updateComment(Long id, Comment updateComment) {
+        Comment existingComment = commentRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Comment not found"));
+
+        existingComment.setContent(updateComment.getContent());
+
+        return commentRepository.save(existingComment);
+    }
+
+    public boolean deleteComment(Long id) {
+        if (commentRepository.existsById(id)) {
+            commentRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 
     private CommentDto convertToDTO(Comment comment) {
