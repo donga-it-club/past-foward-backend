c49257a888d1aea2372fe13ae39047e8cd122eac
llcodingll <ll.o.llwhkr@gmail.com>
2024-03-23 23:38:31 +0900

    feat: 리포지토리 추가

--- src/main/java/aws/retrospective/repository/CommentRepository.java
+++ src/main/java/aws/retrospective/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package aws.retrospective.repository;
 
 import aws.retrospective.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
