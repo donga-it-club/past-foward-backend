eea48468675c824aa6091bdca33d873f0ff72338
백민정 <chungchung3701@gmail.com>
2024-03-23 22:50:53 +0900

    fix: InviteTeamMemberController 수정

--- src/main/java/aws/retrospective/controller/InviteTeamMemberController.java
+++ src/main/java/aws/retrospective/controller/InviteTeamMemberController.java
@@ -1,19 +1,23 @@
 package aws.retrospective.controller;
 
 
+import aws.retrospective.common.CommonApiResponse;
 import aws.retrospective.dto.InviteTeamMemberDTO;
 import aws.retrospective.service.InviteTeamMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class InviteTeamMemberController {
 
     @Autowired
     private InviteTeamMemberService inviteTeamMemberService;
+    private final InviteTeamMemberService inviteTeamMemberService;
 
     @GetMapping("/teams/{teamId}/invitation")
     public ResponseEntity<InviteTeamMemberDTO> getInvitation(@PathVariable("teamId") String teamId) {
@@ -22,6 +26,11 @@ public class InviteTeamMemberController {
         // 초대 링크 및 초대 QR 생성
         inviteTeamMemberService.generateInvitationQR(inviteTeamMemberDTO.getInvitationUrl());
         return ResponseEntity.ok(inviteTeamMemberDTO);
+    public ResponseEntity<CommonApiResponse<InviteTeamMemberDTO>> getInvitation(@PathVariable("teamId") Long teamId) {
+        CommonApiResponse<InviteTeamMemberDTO> response = inviteTeamMemberService.generateInvitation(teamId);
+        return ResponseEntity.ok(response);
     }
 
 }
+
+
