333fa4043b46f3b278593885833393d66544a0d0
llcodingll <ll.o.llwhkr@gmail.com>
2024-03-23 23:38:20 +0900

    feat: Dto 추가

---
+++ src/main/java/aws/retrospective/dto/CommentDto.java
@@ -0,0 +1,33 @@
+package aws.retrospective.dto;
+
+public class CommentDto {
+
+    private Long id;
+
+    private String comment;
+
+    public CommentDto() {
+
+    }
+
+    public CommentDto(Long id, String comment) {
+        this.id = id;
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
