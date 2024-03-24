e4d6cb74f3f4b92daea3736fbe3207c09429a311
백민정 <chungchung3701@gmail.com>
2024-03-24 20:52:00 +0900

    feat: InviteTeamMemberController 추가

---
+++ src/main/java/aws/retrospective/controller/InviteTeamMemberController.java
@@ -0,0 +1,23 @@
+package aws.retrospective.controller;
+
+import aws.retrospective.common.CommonApiResponse;
+import aws.retrospective.dto.InviteTeamMemberDTO;
+import aws.retrospective.service.InviteTeamMemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class InviteTeamMemberController {
+
+    private final InviteTeamMemberService inviteTeamMemberService;
+
+    @GetMapping("/teams/{teamId}/invitation")
+    public ResponseEntity<CommonApiResponse<InviteTeamMemberDTO>> getInvitation(@PathVariable(required = false) Long teamId) {
+        CommonApiResponse<InviteTeamMemberDTO> response = inviteTeamMemberService.generateInvitation(teamId);
+        return ResponseEntity.ok(response);
+    }
+}
