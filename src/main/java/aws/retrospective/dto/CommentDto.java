6b706c73ef5a7066186a591b92d616e9935e3bc6
llcodingll <ll.o.llwhkr@gmail.com>
2024-03-26 19:30:58 +0900

    refactor: CommentDto 리펙토링

--- src/main/java/aws/retrospective/dto/CommentDto.java
+++ src/main/java/aws/retrospective/dto/CommentDto.java
@@ -2,19 +2,7 @@ package aws.retrospective.dto;
 
 import lombok.Getter;
 
-public class CommentDto {
+@Getter
+public record CommentDto(Long id, String content) {
 
-    @Getter
-    private final Long id;
-
-    private final String content;
-
-    public CommentDto(Long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
-
-    public String getComment() {
-        return content;
-    }
 }
