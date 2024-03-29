b4e40600a48dee6cc771d57018b33f51c5fef04a
w1shope <wishhopelee@gmail.com>
2024-03-30 01:55:06 +0900

    feat: 스프링 빈 추가

--- src/main/java/aws/retrospective/RetrospectiveApplication.java
+++ src/main/java/aws/retrospective/RetrospectiveApplication.java
@@ -1,7 +1,10 @@
 package aws.retrospective;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -11,5 +14,10 @@ public class RetrospectiveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RetrospectiveApplication.class, args);
 	}
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 
 }
