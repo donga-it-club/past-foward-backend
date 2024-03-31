498f8c4cec78ba7d2fdd0b8bcbc709e8350e6fcf
llcodingll <ll.o.llwhkr@gmail.com>
2024-03-31 21:15:14 +0900

    refactor: CommentRepository lint

--- src/main/java/aws/retrospective/repository/CommentRepository.java
+++ src/main/java/aws/retrospective/repository/CommentRepository.java
@@ -2,7 +2,6 @@ package aws.retrospective.repository;
 
 import aws.retrospective.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
