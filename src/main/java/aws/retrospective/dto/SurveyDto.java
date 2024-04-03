8a96d7fb6f4e071ad237051eb70f427025ae16d6
llcodingll <ll.o.llwhkr@gmail.com>
2024-04-03 22:57:23 +0900

    refactor: SurveyDto 빌더 수정, @Setter 삭제

--- src/main/java/aws/retrospective/dto/SurveyDto.java
+++ src/main/java/aws/retrospective/dto/SurveyDto.java
@@ -2,75 +2,16 @@ package aws.retrospective.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 public class SurveyDto {
 
-    private Long id;
-    private String age;
-    private String gender;
-    private String occupation;
-    private String region;
-    private String source;
-    private String purpose;
-
-    public static SurveyDtoBuilder builder() {
-        return new SurveyDtoBuilder();
-    }
-
-    public static class SurveyDtoBuilder {
-
-        private Long id;
-        private String age;
-        private String gender;
-        private String occupation;
-        private String region;
-        private String source;
-        private String purpose;
-
-        SurveyDtoBuilder() {
-        }
-
-        public SurveyDtoBuilder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public SurveyDtoBuilder age(String age) {
-            this.age = age;
-            return this;
-        }
-
-        public SurveyDtoBuilder gender(String gender) {
-            this.gender = gender;
-            return this;
-        }
-
-        public SurveyDtoBuilder occupation(String occupation) {
-            this.occupation = occupation;
-            return this;
-        }
-
-        public SurveyDtoBuilder region(String region) {
-            this.region = region;
-            return this;
-        }
-
-        public SurveyDtoBuilder source(String source) {
-            this.source = source;
-            return this;
-        }
-
-        public SurveyDtoBuilder purpose(String purpose) {
-            this.purpose = purpose;
-            return this;
-        }
-
-        public SurveyDto build() {
-            return new SurveyDto(id, age, gender, occupation, region, source, purpose);
-        }
-    }
+    private final Long id;
+    private final String age;
+    private final String gender;
+    private final String occupation;
+    private final String region;
+    private final String source;
+    private final String purpose;
 }
