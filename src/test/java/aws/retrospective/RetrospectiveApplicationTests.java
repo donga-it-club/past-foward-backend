bcdd5a4f7b8fae060c09725f126f039e8efcd926
llcodingll <ll.o.llwhkr@gmail.com>
2024-04-03 23:21:21 +0900

    fix: RetrospectiveApplicationTests 수정

--- src/test/java/aws/retrospective/RetrospectiveApplicationTests.java
+++ src/test/java/aws/retrospective/RetrospectiveApplicationTests.java
@@ -2,14 +2,8 @@ package aws.retrospective;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
 class RetrospectiveApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
