5c0b5c4f2428600b400878b1262e3a7abec4c642
zzero-o <2216132@donga.ac.kr>
2024-04-14 02:48:58 +0900

    fix: SurveyController 수정

--- src/main/java/aws/retrospective/controller/SurveyController.java
+++ src/main/java/aws/retrospective/controller/SurveyController.java
@@ -2,15 +2,14 @@ package aws.retrospective.controller;
 
 import aws.retrospective.common.CommonApiResponse;
 
-import aws.retrospective.dto.SurveyDto;
+import aws.retrospective.dto.SaveSurveyDto;
 import aws.retrospective.service.SurveyService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,8 +36,8 @@ public class SurveyController {
 
     @Operation(summary = "설문조사 조회")
     @GetMapping
-    public CommonApiResponse<List<SurveyDto>> getAllSurveys() {
-        List<SurveyDto> surveys = (List<SurveyDto>) surveyService.getAllSurveys();
+    public CommonApiResponse<List<SaveSurveyDto>> getAllSurveys() {
+        List<SaveSurveyDto> surveys = (List<SaveSurveyDto>) surveyService.getAllSurveys();
 
         // 정상적으로 데이터를 조회한 경우 successResponse 메서드로 응답을 구성
         return CommonApiResponse.successResponse(HttpStatus.OK, surveys);
