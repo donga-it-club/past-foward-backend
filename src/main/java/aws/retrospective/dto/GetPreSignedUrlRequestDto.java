34a5bba67ce16fa7bbda932a171fd7bb99561a9e
정민석 <ms.jung@fanmaum.com>
2024-04-13 21:30:30 +0900

    fix: NotEmpty -> NotNull

--- src/main/java/aws/retrospective/dto/GetPreSignedUrlRequestDto.java
+++ src/main/java/aws/retrospective/dto/GetPreSignedUrlRequestDto.java
@@ -1,7 +1,6 @@
 package aws.retrospective.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -12,7 +11,7 @@ public class GetPreSignedUrlRequestDto {
     @Schema(description = "파일명", example = "test.jpg")
     private String filename;
 
-    @NotEmpty(message = "method는 필수 정보입니다.")
+    @NotNull(message = "method는 필수 정보입니다.")
     @Schema(description = "객체 가져올 시 GET, 업로드 시 PUT", example = "GET")
     private PresigendUrlMethod method;
 }
