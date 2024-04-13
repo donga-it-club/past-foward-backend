fbb1349bea8683c12a6ce6be7965bd2ee9dcbd03
정민석 <ms.jung@fanmaum.com>
2024-04-13 19:03:18 +0900

    feat: dto method 필드 추가

--- src/main/java/aws/retrospective/dto/GetPreSignedUrlRequestDto.java
+++ src/main/java/aws/retrospective/dto/GetPreSignedUrlRequestDto.java
@@ -1,6 +1,7 @@
 package aws.retrospective.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -10,4 +11,8 @@ public class GetPreSignedUrlRequestDto {
     @NotNull(message = "파일명은 필수 정보입니다.")
     @Schema(description = "파일명", example = "test.jpg")
     private String filename;
+
+    @NotEmpty(message = "method는 필수 정보입니다.")
+    @Schema(description = "객체 가져올 시 GET, 업로드 시 PUT", example = "GET")
+    private PresigendUrlMethod method;
 }
