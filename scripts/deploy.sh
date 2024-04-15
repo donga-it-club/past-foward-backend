36b331ad18b181726c323af5f83846d4bba2a1d1
정민석 <ms.jung@fanmaum.com>
2024-04-15 22:07:22 +0900

    fix: 로깅 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,42 +2,35 @@
 export SPRING_PROFILES_ACTIVE=prod
 
 BUILD_JAR=$(ls /home/ec2-user/action/build/libs/*.jar)
-JAR_NAME=$(basename "$BUILD_JAR")
+JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
 
-echo "> 현재 실행중인 애플리케이션 PID 확인" >> /home/ec2-user/action/deploy.log
-CURRENT_PID=$(pgrep -fl "$JAR_NAME" | grep -v grep | awk '{print $1}')
+echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
+DEPLOY_PATH=/home/ec2-user/action/
+cp $BUILD_JAR $DEPLOY_PATH
 
-if [ -z "$CURRENT_PID" ]
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
 else
-  echo "> 애플리케이션 종료: PID $CURRENT_PID" >> /home/ec2-user/action/deploy.log
-  kill -15 "$CURRENT_PID"
-  sleep_count=0
-  while kill -0 "$CURRENT_PID" >/dev/null 2>&1; do
-    if [ $sleep_count -ge 10 ]
-    then
-      echo "Waiting for process to terminate... Timeout!" >> /home/ec2-user/action/deploy.log
-      break
-    fi
-    echo "Waiting for process to terminate..." >> /home/ec2-user/action/deploy.log
-    sleep 1
-    sleep_count=$((sleep_count+1))
-  done
-  echo "> 애플리케이션이 종료되었습니다." >> /home/ec2-user/action/deploy.log
-fi
-
-DEPLOY_PATH=/home/ec2-user/action/
-if [ -f "$BUILD_JAR" ]
-then
-  echo "> build 파일을 $DEPLOY_PATH로 복사" >> /home/ec2-user/action/deploy.log
-  cp "$BUILD_JAR" $DEPLOY_PATH
-else
-  echo "> build 파일이 존재하지 않습니다." >> /home/ec2-user/action/deploy.log
-  exit 1
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> $DEPLOY_JAR 배포" >> /home/ec2-user/action/deploy.log
-nohup java -jar "$DEPLOY_JAR" >> /home/ec2-user/deploy.log 2>> /home/ec2-user/action/deploy_err.log &
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+
+echo "> 애플리케이션 시작 확인" >> /home/ec2-user/action/deploy.log
+sleep 10
+CHECK_PID=$(pgrep -f $JAR_NAME)
+if [ -z $CHECK_PID ]
+then
+  echo "> 애플리케이션 시작 실패" >> /home/ec2-user/action/deploy.log
+else
+  echo "> 애플리케이션 시작 성공" >> /home/ec2-user/action/deploy.log
+fi
