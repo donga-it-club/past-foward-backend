2f0238ff5ebd3d37b5e6b2ba1f82a430f30e829b
llcodingll <ll.o.llwhkr@gmail.com>
2024-04-15 12:16:55 +0900

    Update endpoint from /api/comment to /comment

--- src/main/java/aws/retrospective/controller/CommentController.java
+++ src/main/java/aws/retrospective/controller/CommentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/comments")
+@RequestMapping("/comments")
 @RequiredArgsConstructor
 public class CommentController {
 
