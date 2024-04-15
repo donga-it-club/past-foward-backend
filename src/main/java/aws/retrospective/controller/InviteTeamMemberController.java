a99965508cc6280a1c37e8061d395aea70068b6e
백민정 <chungchung3701@gmail.com>
2024-04-15 22:38:59 +0900

    fix: InviteTeamMemberController 수정

--- src/main/java/aws/retrospective/controller/InviteTeamMemberController.java
+++ src/main/java/aws/retrospective/controller/InviteTeamMemberController.java
@@ -5,17 +5,19 @@ import aws.retrospective.dto.InviteTeamMemberDTO;
 import aws.retrospective.service.InviteTeamMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/teams")
 public class InviteTeamMemberController {
 
     private final InviteTeamMemberService inviteTeamMemberService;
 
-    @RequestMapping("/teams/{teamId}/invitation-url")
+    @GetMapping("/{teamId}/invitation-url")
     public CommonApiResponse<InviteTeamMemberDTO> getInvitation(@PathVariable Long teamId) { // 팀 ID에 해당하는 초대 정보를 가져오는 메서드
         // 팀 ID를 기반으로 초대 정보를 생성하고, 그 결과를 받아옴
         InviteTeamMemberDTO inviteTeamMemberDTO = inviteTeamMemberService.generateInvitation(teamId);
