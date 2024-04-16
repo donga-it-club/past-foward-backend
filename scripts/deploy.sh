9bd54b6b78e32c8532f5f26c0a0834f6d145545f
정민석 <ms.jung@fanmaum.com>
2024-04-16 20:16:57 +0900

    feat: add graceful shutdown

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ cp $BUILD_JAR $DEPLOY_PATH
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z "$CURRENT_PID" ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
 else
@@ -20,14 +20,13 @@ else
   kill -15 $CURRENT_PID
   sleep 10
 
-  if ! ps -p $CURRENT_PID > /dev/null
-  then
-    echo "> 애플리케이션이 성공적으로 종료되었습니다." >> /home/ec2-user/action/deploy.log
-  else
-    echo "> 애플리케이션 종료 실패, 재시도" >> /home/ec2-user/action/deploy.log
-    kill -9 $CURRENT_PID
+  while ps -p $CURRENT_PID > /dev/null; do
+    echo "> 애플리케이션 종료 대기 중..." >> /home/ec2-user/action/deploy.log
+    kill -15 $CURRENT_PID
     sleep 5
-  fi
+  done
+
+  echo "> 애플리케이션이 성공적으로 종료되었습니다." >> /home/ec2-user/action/deploy.log
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
@@ -37,7 +36,7 @@ nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action
 echo "$(date) > 애플리케이션 시작 확인" >> /home/ec2-user/action/deploy.log
 sleep 10
 CHECK_PID=$(pgrep -f $JAR_NAME)
-if [ -z $CHECK_PID ]
+if [ -z "$CHECK_PID" ]
 then
   echo "$(date) > 애플리케이션 시작 실패" >> /home/ec2-user/action/deploy.log
 else
