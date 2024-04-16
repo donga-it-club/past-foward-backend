f2eab36c605ee2eeb003b9df1126c88583bbe3a3
정민석 <ms.jung@fanmaum.com>
2024-04-16 18:27:40 +0900

    feat: add     @Enumerated(EnumType.STRING)
    TODO: Attribute Converter

--- src/main/java/aws/retrospective/entity/Survey.java
+++ src/main/java/aws/retrospective/entity/Survey.java
@@ -2,6 +2,8 @@ package aws.retrospective.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,10 +24,16 @@ public class Survey extends BaseEntity {
 
     // 설문조사 관련 필드 추가
     private String age;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
     private String occupation;
+
     private String region;
+
     private String source;
+
     private String purpose;
 
     public enum Gender {
