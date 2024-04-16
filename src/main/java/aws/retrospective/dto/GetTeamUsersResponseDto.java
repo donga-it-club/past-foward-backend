f4b9624bf09ba2022b28aa1f19e13fd6b6618f58
w1shope <wishhopelee@gmail.com>
2024-04-16 18:25:16 +0900

    feat: 팀원 조회 응답 필드 추가

--- src/main/java/aws/retrospective/dto/GetTeamUsersResponseDto.java
+++ src/main/java/aws/retrospective/dto/GetTeamUsersResponseDto.java
@@ -2,9 +2,13 @@ package aws.retrospective.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class GetTeamUsersResponseDto {
 
     @Schema(description = "사용자 id", example = "1")
@@ -16,10 +20,18 @@ public class GetTeamUsersResponseDto {
     @Schema(description = "프로필 이미지", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
     private String profileImage;
 
+    @Schema(description = "이메일", example = "test@gmail.com")
+    private String email;
+
+    @Schema(description = "참여 일자", example = "2021-07-01T00:00:00")
+    private LocalDateTime joinedAt;
+
     @QueryProjection
-    public GetTeamUsersResponseDto(Long userId, String username, String profileImageUrl) {
+    public GetTeamUsersResponseDto(Long userId, String username, String profileImageUrl, String email, LocalDateTime joinedAt) {
         this.userId = userId;
         this.username = username;
         this.profileImage = profileImageUrl;
+        this.email = email;
+        this.joinedAt = joinedAt;
     }
 }
