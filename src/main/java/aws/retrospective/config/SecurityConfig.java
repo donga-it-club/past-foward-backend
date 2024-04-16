077abb0c21ff43e4c3308216044bfd15353d5f30
정민석 <ms.jung@fanmaum.com>
2024-04-17 00:49:25 +0900

    feat: 인증 필요없는 엔드포인트 정의

--- src/main/java/aws/retrospective/config/SecurityConfig.java
+++ src/main/java/aws/retrospective/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package aws.retrospective.config;
 import aws.retrospective.common.CustomAuthenticationConverter;
 import aws.retrospective.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,7 +25,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .anyRequest().authenticated()  // 모든 요청에 대해 인증을 요구
+            .requestMatchers("/api/**", "/health", "/swagger-ui/**", "/swagger-resources/**",
+                "/api-docs/**")
+            .permitAll()
+            .anyRequest().authenticated()
             .and()
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt
