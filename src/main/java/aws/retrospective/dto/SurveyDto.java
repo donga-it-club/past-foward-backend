805b7d784751ed726baabfae764f36d41b723374
zzero-o <2216132@donga.ac.kr>
2024-04-16 23:53:32 +0900

    feat: SurveyDto 추가

---
+++ src/main/java/aws/retrospective/dto/SurveyDto.java
@@ -0,0 +1,34 @@
+package aws.retrospective.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Builder
+public class SurveyDto {
+    private Long surveyId;
+
+    @NotNull
+    private Integer age;
+
+    @NotEmpty
+    private String gender;
+
+    @NotEmpty
+    private String occupation;
+
+    @NotEmpty
+    private String region;
+
+    @NotEmpty
+    private String source;
+
+    private String purpose;
+}
