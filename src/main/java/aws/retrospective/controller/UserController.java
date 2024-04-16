ed301872c1f17ce33c843bc951507a181c484198
정민석 <ms.jung@fanmaum.com>
2024-04-17 00:48:21 +0900

    feat: 유저 정보 조회 API 토큰 받도록 수정

--- src/main/java/aws/retrospective/controller/UserController.java
+++ src/main/java/aws/retrospective/controller/UserController.java
@@ -1,13 +1,16 @@
 package aws.retrospective.controller;
 
 import aws.retrospective.common.CommonApiResponse;
+import aws.retrospective.common.CurrentUser;
 import aws.retrospective.dto.GetUserInfoDto;
 import aws.retrospective.dto.UpdateUserProfileRequestDto;
 import aws.retrospective.dto.UpdateUserProfileResponseDto;
+import aws.retrospective.entity.User;
 import aws.retrospective.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -30,23 +33,19 @@ public class UserController {
     // 프로필 이미지 등록
     @PutMapping("/{userId}/thumbnail")
     @Operation(summary = "프로필 이미지 등록")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "프로필 이미지 등록 성공"),
-    })
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "프로필 이미지 등록 성공"),})
     public CommonApiResponse<UpdateUserProfileResponseDto> updateProfile(@PathVariable Long userId,
         @RequestBody @Valid UpdateUserProfileRequestDto request) {
-        UpdateUserProfileResponseDto response = userService.updateProfile(
-            userId, request);
+        UpdateUserProfileResponseDto response = userService.updateProfile(userId, request);
         return CommonApiResponse.successResponse(HttpStatus.OK, response);
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/me")
+    @SecurityRequirement(name = "JWT")
     @Operation(summary = "유저 정보 조회")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "유저 정보 조회 성공"),
-    })
-    public CommonApiResponse<GetUserInfoDto> getUserInfo(@PathVariable Long userId) {
-        GetUserInfoDto response = userService.getUserInfo(userId);
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "유저 정보 조회 성공"),})
+    public CommonApiResponse<GetUserInfoDto> getUserInfo(@CurrentUser User user) {
+        GetUserInfoDto response = userService.getUserInfo(user.getId());
         return CommonApiResponse.successResponse(HttpStatus.OK, response);
     }
 }
