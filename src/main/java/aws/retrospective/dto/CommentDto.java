074726e5a92bb38414794dc081e71a97dbb48024
llcodingll <ll.o.llwhkr@gmail.com>
2024-04-16 21:02:04 +0900

    feat: Dto totalCount 생성

--- src/main/java/aws/retrospective/dto/CommentDto.java
+++ src/main/java/aws/retrospective/dto/CommentDto.java
@@ -3,13 +3,6 @@ package aws.retrospective.dto;
 import lombok.Getter;
 
 @Getter
-public class CommentDto {
+public record CommentDto(Long id, String content, int totalCount) {
 
-    private Long id;
-    private String content;
-
-    public CommentDto(Long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
 }
