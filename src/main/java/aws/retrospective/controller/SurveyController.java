3147b8b78cc2214425aa7884cbd7f649f3fd8d3e
zzero-o <2216132@donga.ac.kr>
2024-04-16 23:52:54 +0900

    fix: SuveyContrroller 수정

--- src/main/java/aws/retrospective/controller/SurveyController.java
+++ src/main/java/aws/retrospective/controller/SurveyController.java
@@ -2,7 +2,7 @@ package aws.retrospective.controller;
 
 import aws.retrospective.common.CommonApiResponse;
 
-import aws.retrospective.dto.SaveSurveyDto;
+import aws.retrospective.dto.SurveyDto;
 import aws.retrospective.service.SurveyService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,7 +10,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -23,7 +22,7 @@ public class SurveyController {
 
     @PostMapping("/{surveyId}/response")
     @Operation(summary = "설문조사 저장")
-    public CommonApiResponse<String> addSurvey(@Valid @RequestBody SaveSurveyDto surveyDto) {
+    public CommonApiResponse<String> addSurvey(@Valid @RequestBody SurveyDto surveyDto) {
         try {
             surveyService.addSurvey(surveyDto);
             return (CommonApiResponse.successResponse(HttpStatus.OK, "Survey added succesfully"));
@@ -35,8 +34,8 @@ public class SurveyController {
 
     @Operation(summary = "설문조사 조회")
     @GetMapping
-    public CommonApiResponse<List<SaveSurveyDto>> getAllSurveys() {
-        List<SaveSurveyDto> surveys = (List<SaveSurveyDto>) surveyService.getAllSurveys();
+    public CommonApiResponse<List<SurveyDto>> getAllSurveys() {
+        List<SurveyDto> surveys = (List<SurveyDto>) surveyService.getAllSurveys();
 
         // 정상적으로 데이터를 조회한 경우 successResponse 메서드로 응답을 구성
         return CommonApiResponse.successResponse(HttpStatus.OK, surveys);
