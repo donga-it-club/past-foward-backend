ad13621e9ef397544bed82f57eeacadd9a7edd17
정민석 <ms.jung@fanmaum.com>
2024-04-17 23:50:24 +0900

    chore: delete contextLoads

--- src/test/java/aws/retrospective/RetrospectiveApplicationTests.java
+++ src/test/java/aws/retrospective/RetrospectiveApplicationTests.java
@@ -1,6 +1,5 @@
 package aws.retrospective;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -8,7 +7,5 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("local")
 class RetrospectiveApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 }
