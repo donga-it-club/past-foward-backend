fc52a54614b72c75016f0a5204672fbbb9940d86
w1shope <wishhopelee@gmail.com>
2024-04-18 02:12:56 +0900

    fix: 쿼리 파라미터로 받도록 수정

--- src/main/java/aws/retrospective/controller/TeamController.java
+++ src/main/java/aws/retrospective/controller/TeamController.java
@@ -1,21 +1,19 @@
 package aws.retrospective.controller;
 
 import aws.retrospective.common.CommonApiResponse;
-import aws.retrospective.dto.GetTeamUsersRequestDto;
 import aws.retrospective.dto.GetTeamUsersResponseDto;
 import aws.retrospective.service.TeamService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,8 +29,8 @@ public class TeamController {
     @ApiResponses(value = {@ApiResponse(responseCode = "200")})
     @GetMapping("/{teamId}/users")
     public CommonApiResponse<List<GetTeamUsersResponseDto>> getTeamMembers(
-        @PathVariable Long teamId, @RequestBody @Valid GetTeamUsersRequestDto request) {
-        List<GetTeamUsersResponseDto> response = teamService.getTeamMembers(teamId, request);
+        @PathVariable Long teamId, @RequestParam Long retrospectiveId) {
+        List<GetTeamUsersResponseDto> response = teamService.getTeamMembers(teamId, retrospectiveId);
         return CommonApiResponse.successResponse(HttpStatus.OK, response);
     }
 }
