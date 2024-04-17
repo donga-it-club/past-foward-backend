b83374e2b3a0f64efafc28a5ad9b3148041c227a
정민석 <ms.jung@fanmaum.com>
2024-04-17 22:34:57 +0900

    fix: 불필요한 메서드 제거

--- src/test/java/aws/retrospective/service/BookmarkServiceTest.java
+++ src/test/java/aws/retrospective/service/BookmarkServiceTest.java
@@ -49,7 +49,6 @@ public class BookmarkServiceTest {
         ReflectionTestUtils.setField(retrospective, "id", 1L);
         ReflectionTestUtils.setField(user, "id", 1L);
 
-        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
         when(retrospectiveRepository.findById(retrospective.getId())).thenReturn(
             Optional.of(retrospective));
 
@@ -77,7 +76,6 @@ public class BookmarkServiceTest {
         ReflectionTestUtils.setField(retrospective, "id", 1L);
         ReflectionTestUtils.setField(user, "id", 1L);
 
-        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
         when(retrospectiveRepository.findById(retrospective.getId())).thenReturn(
             Optional.of(retrospective));
         when(bookmarkRepository.findByUserIdAndRetrospectiveId(user.getId(),
