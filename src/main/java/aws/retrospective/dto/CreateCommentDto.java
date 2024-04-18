ff83832d09018232e05aa802f1594fde458bf8ec
llcodingll <ll.o.llwhkr@gmail.com>
2024-04-18 19:47:07 +0900

    refactor: CreateCommentDto

--- src/main/java/aws/retrospective/dto/CreateCommentDto.java
+++ src/main/java/aws/retrospective/dto/CreateCommentDto.java
@@ -1,12 +1,14 @@
 package aws.retrospective.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CreateCommentDto {
+
     @Schema(description = "user id", example = "1")
     private Long userId;
 
@@ -16,9 +18,5 @@ public class CreateCommentDto {
     @Schema(description = "댓글 내용", example = "새로운 댓글입니다.")
     private String commentContent;
 
-    public CreateCommentDto(Long userId, Long commentSectionId, String commentContent) {
-        this.userId = userId;
-        this.commentSectionId = commentSectionId;
-        this.commentContent = commentContent;
-    }
+    private LocalDateTime createDate;
 }
