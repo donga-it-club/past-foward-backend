a7a669919bc4e5c485f4910fd80152bcd614359e
정민석 <ms.jung@fanmaum.com>
2024-04-18 23:14:55 +0900

    fix: 잘못된 반환값 수정

--- src/main/java/aws/retrospective/repository/TeamInvitationRepository.java
+++ src/main/java/aws/retrospective/repository/TeamInvitationRepository.java
@@ -1,10 +1,10 @@
 package aws.retrospective.repository;
 
 import aws.retrospective.entity.TeamInvite;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+public interface TeamInvitationRepository extends JpaRepository<TeamInvite, Long> {
 
-public interface TeamInvitationRepository extends JpaRepository<TeamInvite,Long> {
-    Optional<Object> findByInvitationCode(String invitationCode);
+    Optional<TeamInvite> findByInvitationCode(String invitationCode);
 }
