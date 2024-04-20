227dfa3972558d0e6192455901cbc9fe4e61520e
정민석 <ms.jung@fanmaum.com>
2024-04-20 17:07:11 +0900

    fix: 불필요한 dto 필드를 제거합니다

--- src/main/java/aws/retrospective/dto/CreateCommentDto.java
+++ src/main/java/aws/retrospective/dto/CreateCommentDto.java
@@ -2,6 +2,7 @@ package aws.retrospective.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,10 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateCommentDto {
 
-    @Schema(description = "comment id", example = "1")
-    private Long commentId;
+    @NotNull
     @Schema(description = "section id", example = "2")
     private Long sectionId;
+
     @NotEmpty(message = "댓글 내용은 필수 입력 값입니다.")
     @Schema(description = "댓글 내용", example = "여러분 프로젝트하느라 수고 많으셨습니다.")
     private String commentContent;
