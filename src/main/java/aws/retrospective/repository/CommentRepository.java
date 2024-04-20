64ef10f66246a79ae41857d2b87cbfee48c62b13
llcodingll <ll.o.llwhkr@gmail.com>
2024-04-20 12:02:01 +0900

    refactor: commentRepository

--- src/main/java/aws/retrospective/repository/CommentRepository.java
+++ src/main/java/aws/retrospective/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package aws.retrospective.repository;
 
 import aws.retrospective.entity.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom{
 
-
+    @Query("select c from Comment c join c.section cs where sc.id = :sectionId")
+    List<Comment> getCommentsWithSections(@Param("sectionId") Long sectionId);
 }
