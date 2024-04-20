5725aab2c35591b7e6d1fa13b2800a95c93b5557
정민석 <ms.jung@fanmaum.com>
2024-04-20 17:06:28 +0900

    feat: findCommentsBySectionId 추가

--- src/main/java/aws/retrospective/repository/CommentRepository.java
+++ src/main/java/aws/retrospective/repository/CommentRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom{
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("select c from Comment c join c.section cs where sc.id = :sectionId")
-    List<Comment> getCommentsWithSections(@Param("sectionId") Long sectionId);
+    @Query("select c from Comment c join c.section cs where cs.id = :sectionId")
+    List<Comment> findCommentsBySectionId(@Param("sectionId") Long sectionId);
 }
