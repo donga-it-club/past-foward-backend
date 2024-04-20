282e4282e3b4a48c317759a977ebe86c9612d2e1
정민석 <ms.jung@fanmaum.com>
2024-04-20 17:42:35 +0900

    feat: UserTeamRepository  메서드들 추가

--- src/main/java/aws/retrospective/repository/UserTeamRepository.java
+++ src/main/java/aws/retrospective/repository/UserTeamRepository.java
@@ -1,8 +1,15 @@
 package aws.retrospective.repository;
 
 import aws.retrospective.entity.UserTeam;
+import aws.retrospective.entity.UserTeamRole;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserTeamRepository extends JpaRepository<UserTeam, Long>, UserTeamRepositoryCustom {
+public interface UserTeamRepository extends JpaRepository<UserTeam, Long>,
+    UserTeamRepositoryCustom {
+
+    public void deleteByTeamIdAndUserId(Long teamId, Long userId);
+
+    public Optional<UserTeam> findByTeamIdAndRole(Long teamId, UserTeamRole role);
 
 }
