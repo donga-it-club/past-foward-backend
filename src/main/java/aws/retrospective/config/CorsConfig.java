1faaa7f30c02a68fdd732b70ef94c510499c21e5
정민석 <ms.jung@fanmaum.com>
2024-04-20 21:10:56 +0900

    Update CorsConfig.java

--- src/main/java/aws/retrospective/config/CorsConfig.java
+++ src/main/java/aws/retrospective/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package aws.retrospective.config;
 
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +19,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin(Arrays.toString(allowedOrigins));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
