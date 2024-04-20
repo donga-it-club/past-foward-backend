e38fca3e799b52ecb90593a1c60237c21df9c4b2
정민석 <ms.jung@fanmaum.com>
2024-04-20 22:59:26 +0900

    Update SecurityConfig.java

--- src/main/java/aws/retrospective/config/SecurityConfig.java
+++ src/main/java/aws/retrospective/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -33,34 +34,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()));
-        http.authorizeHttpRequests((authorizeHttpRequests) ->
-                authorizeHttpRequests
-                    .requestMatchers("/api/auth/**").permitAll()
-                    .requestMatchers("/v3/api-docs/**").permitAll()
-                    .requestMatchers("/swagger-ui/**").permitAll()
-                    .requestMatchers("/swagger-ui.html").permitAll()
-                    .requestMatchers("/api-docs/**").permitAll()
-                    .requestMatchers("/api/**").permitAll()
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        http.authorizeHttpRequests(
+                (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/v3/**",
+                        "/swagger-ui/**", "/swagger-ui.html", "/api-docs/**", "/api/**", "/swaaagger-ui/**")
+                    .permitAll()
+                    .anyRequest().authenticated()
 
-                    .requestMatchers("/comments/**").authenticated()
-                    .requestMatchers("/mails/**").authenticated()
-                    .requestMatchers("/retrospectives/**").authenticated()
-                    .requestMatchers("/retrospective-templates/**").authenticated()
-                    .requestMatchers("/s3/**").authenticated()
-                    .requestMatchers("/sections/**").authenticated()
-                    .requestMatchers("/surveys/**").authenticated()
-                    .requestMatchers("/teams/**").authenticated()
-                    .requestMatchers("/users/**").authenticated()
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
             )
-            .csrf((csrf) -> csrf.disable())
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt
-                    .decoder(JwtDecoders.fromOidcIssuerLocation(issuerUri))
-                    .jwtAuthenticationConverter(customAuthenticationConverter()))
-            );
+            .csrf((csrf) -> csrf.disable()).oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                jwt -> jwt.decoder(JwtDecoders.fromOidcIssuerLocation(issuerUri))
+                    .jwtAuthenticationConverter(customAuthenticationConverter())));
         return http.build();
     }
 
