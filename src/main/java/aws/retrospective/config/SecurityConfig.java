09650e976517a941b4fc5392e9656ad923c6f425
정민석 <ms.jung@fanmaum.com>
2024-04-24 23:02:22 +0900

    fix: 헬스체크 엔드포인트 추가

--- src/main/java/aws/retrospective/config/SecurityConfig.java
+++ src/main/java/aws/retrospective/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
         http.authorizeHttpRequests(
-            (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/health",
+            (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/actuator/health",
                     "/swagger-ui/**", "/swagger-ui.html", "/api-docs/**", "/api/**",
                     "/swagger-resources/**", "/swagger-ui.html").permitAll().anyRequest()
                 .authenticated()
