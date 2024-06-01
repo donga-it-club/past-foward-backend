08eb0945da07337c01d5ca0b97a336b82d012e57
w1shope <wishhopelee@gmail.com>
2024-06-01 09:33:25 +0900

    fix: 2개 이상의 kudos 회고 카드에 동일한 사용자를 지정할 수 있다

--- src/main/java/aws/retrospective/entity/KudosTarget.java
+++ src/main/java/aws/retrospective/entity/KudosTarget.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -26,7 +27,7 @@ public class KudosTarget extends BaseEntity{
     @JoinColumn(name = "section_id")
     private Section section;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
