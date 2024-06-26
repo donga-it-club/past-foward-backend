d8f476e97b7020d15a0d826592ccd6cb3415943a
백민정 <chungchung3701@gmail.com>
2024-06-26 18:41:01 +0900

    fix : 사용자의 관리자 권한을 업데이트하는 메서드

--- src/main/java/aws/retrospective/service/UserService.java
+++ src/main/java/aws/retrospective/service/UserService.java
@@ -1,5 +1,6 @@
 package aws.retrospective.service;
 
+import aws.retrospective.dto.AdminRoleDTO;
 import aws.retrospective.dto.GetUserInfoDto;
 import aws.retrospective.dto.UpdateUserProfileRequestDto;
 import aws.retrospective.dto.UpdateUserProfileResponseDto;
@@ -33,9 +34,22 @@ public class UserService {
             currentUser.getCreatedDate(), currentUser.getUpdatedDate());
     }
 
+    @Transactional
+    public void updateAdminStatus(User user, AdminRoleDTO adminRoleDTO) {
+        user.updateAdministrator(adminRoleDTO.isAdmin());
+        userRepository.save(user);
+    }
+
 
     private User getUser(Long userId) {
         return userRepository.findById(userId)
             .orElseThrow(() -> new NoSuchElementException("사용자를 조회할 수 없습니다. id = " + userId));
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isAdmin(String email) {
+        return userRepository.findByEmail(email)
+                .map(User::isAdministrator)
+                .orElse(false);
     }
 }
