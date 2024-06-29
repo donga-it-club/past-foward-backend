45603a642c9273cfa3b5b9fa8a17a8d91acfac56
백민정 <chungchung3701@gmail.com>
2024-06-29 21:43:55 +0900

    fix : @AllArgsConstructor 추가

--- src/main/java/aws/retrospective/dto/NoticeBoardWritingResponseDto.java
+++ src/main/java/aws/retrospective/dto/NoticeBoardWritingResponseDto.java
@@ -2,23 +2,17 @@ package aws.retrospective.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class NoticeBoardWritingResponseDto {
     private String title;
     private String content;
     private String status;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
-
-    public NoticeBoardWritingResponseDto(String title, String content, String status, LocalDateTime createdDate, LocalDateTime modifiedDate) {
-        this.title = title;
-        this.content = content;
-        this.status = status;
-        this.createdDate = createdDate;
-        this.modifiedDate = modifiedDate;
-    }
 }
