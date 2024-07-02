aecc96d9049a521caa43a3d5e2a1e3d9ab56658b
백민정 <chungchung3701@gmail.com>
2024-07-02 23:59:21 +0900

    fix : @PreAuthorize 사용

--- src/main/java/aws/retrospective/config/SecurityConfig.java
+++ src/main/java/aws/retrospective/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,6 +20,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true) // prePostEnabled => PreAuthorize 어노테이션 사용 여부.
 @RequiredArgsConstructor
 public class SecurityConfig {
 
--- src/main/java/aws/retrospective/controller/NoticeBoardWritingAdminController.java
+++ src/main/java/aws/retrospective/controller/NoticeBoardWritingAdminController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -32,10 +33,8 @@ public class NoticeBoardWritingAdminController {
     @Operation(summary = "게시글 작성", description = "새로운 게시글을 작성하는 API")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "게시글 작성 성공")})
     @PostMapping("/posts")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public CommonApiResponse<NoticeBoardWritingResponseDto> createPost(@CurrentUser User user, @RequestBody @Valid NoticeBoardWritingRequestDto requestDto) {
-        if (!user.isAdministrator()) {
-            throw new ForbiddenAccessException("글쓰기 권한이 없습니다.");
-        }
         NoticeBoardWritingResponseDto responseDto = noticeBoardWritingService.savePost(requestDto);
         return CommonApiResponse.successResponse(HttpStatus.OK, responseDto);
     }
@@ -43,10 +42,8 @@ public class NoticeBoardWritingAdminController {
     @Operation(summary = "임시 게시글 저장", description = "임시 게시글을 저장하는 API")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "임시 게시글 저장 성공")})
     @PostMapping("/temp-posts")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public CommonApiResponse<NoticeBoardWritingResponseDto> saveTempPost(@CurrentUser User user, @RequestBody @Valid NoticeBoardWritingRequestDto requestDto) {
-        if (!user.isAdministrator()) {
-            throw new ForbiddenAccessException("글쓰기 권한이 없습니다.");
-        }
         NoticeBoardWritingResponseDto responseDto = noticeBoardWritingService.saveTempPost(requestDto);
         return CommonApiResponse.successResponse(HttpStatus.OK, responseDto);
     }
@@ -70,10 +67,8 @@ public class NoticeBoardWritingAdminController {
     @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "게시글 삭제 성공")})
     @DeleteMapping("/posts/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public void deletePost(@CurrentUser User user, @PathVariable Long id) {
-        if (!user.isAdministrator()) {
-            throw new ForbiddenAccessException("삭제 권한이 없습니다.");
-        }
         noticeBoardWritingService.deletePost(id);
     }
 }
