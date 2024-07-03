37f8e2699d734d537af31752c53e6c7890025ed1
zzero-o <2216132@donga.ac.kr>
2024-07-04 06:23:32 +0900

    Feat: SurveyServiceTest 설문조사 섹션 별 분할

--- src/test/java/aws/retrospective/service/SurveyServiceTest.java
+++ src/test/java/aws/retrospective/service/SurveyServiceTest.java
@@ -1,7 +1,7 @@
 package aws.retrospective.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import aws.retrospective.dto.SurveyDto;
 import aws.retrospective.entity.Survey;
@@ -12,11 +12,14 @@ import aws.retrospective.util.TestUtil;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -59,6 +62,7 @@ public class SurveyServiceTest {
         assertEquals("Korea", surveyDto.getRegion());
         assertEquals("internet", surveyDto.getSource());
         assertEquals(purposes, surveyDto.getPurposes());
+        assertFalse(surveyDto.getEmailConsent());
     }
 
     @Test
@@ -84,5 +88,103 @@ public class SurveyServiceTest {
         // 결과 확인
         assertEquals(surveys.size(), response.size());
         // 추가적인 검증을 원하는 경우 surveyDtos 내용을 검사할 수 있음
+    }
+
+    @Test
+    public void testGetGenderAndAgeSurveys() {
+        // 가짜 데이터 생성
+        List<String> purposes = Arrays.asList("purpose1", "purpose2", "purpose3");
+        List<Survey> surveys = new ArrayList<>();
+        surveys.add(Survey.builder()
+                .age(30)
+                .gender(String.valueOf(Gender.valueOf("MALE")))
+                .occupation("Engineer")
+                .region("Seoul")
+                .source("Internet")
+                .purposes(purposes)
+                .build());
+
+        // Mock 객체 설정
+        when(surveyRepository.findAll()).thenReturn(surveys);
+
+        // 테스트 수행
+        List<SurveyDto> surveyDtos = surveyService.getGenderAndAgeSurveys();
+
+        // 결과 검증
+        assertEquals(1, surveyDtos.size());
+
+        SurveyDto surveyDto = surveyDtos.get(0);
+        assertEquals(30, surveyDto.getAge());
+        assertEquals("MALE", surveyDto.getGender());
+
+        // findAll 메서드가 한 번 호출되었는지 검증
+        verify(surveyRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testGetOccupationAndRegionSurveys() {
+        // 가짜 데이터 생성
+        List<String> purposes = Arrays.asList("purpose1", "purpose2", "purpose3");
+        List<Survey> surveys = new ArrayList<>();
+        surveys.add(Survey.builder()
+                .age(30)
+                .gender("MALE")
+                .occupation("Engineer")
+                .region("Seoul")
+                .source("Internet")
+                .purposes(purposes)
+                .build());
+
+        // Mock 객체 설정
+        when(surveyRepository.findAll()).thenReturn(surveys);
+
+        // 테스트 수행
+        List<SurveyDto> surveyDtos = surveyService.getOccupationAndRegionSurveys();
+
+        // 결과 검증
+        assertEquals(1, surveyDtos.size());
+
+        SurveyDto surveyDto = surveyDtos.get(0);
+        assertEquals("Engineer", surveyDto.getOccupation());
+        assertEquals("Seoul", surveyDto.getRegion());
+
+        // findAll 메서드가 한 번 호출되었는지 검증
+        verify(surveyRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testGetSourceAndPurposeSurveys() {
+        // 가짜 데이터 생성
+        List<String> purposes = Arrays.asList("purpose1", "purpose2", "purpose3");
+        User user = User.builder()
+                .emailConsent(true)
+                .build();
+        List<Survey> surveys = new ArrayList<>();
+        surveys.add(Survey.builder()
+                .age(30)
+                .gender("MALE")
+                .occupation("Engineer")
+                .region("Seoul")
+                .source("Internet")
+                .purposes(purposes)
+                .user(user)
+                .build());
+
+        // Mock 객체 설정
+        when(surveyRepository.findAll()).thenReturn(surveys);
+
+        // 테스트 수행
+        List<SurveyDto> surveyDtos = surveyService.getSourceAndPurposeSurveys();
+
+        // 결과 검증
+        assertEquals(1, surveyDtos.size());
+
+        SurveyDto surveyDto = surveyDtos.get(0);
+        assertEquals("Internet", surveyDto.getSource());
+        assertEquals(purposes, surveyDto.getPurposes());
+        assertEquals(true, surveyDto.getEmailConsent());
+
+        // findAll 메서드가 한 번 호출되었는지 검증
+        verify(surveyRepository, times(1)).findAll();
     }
 }
