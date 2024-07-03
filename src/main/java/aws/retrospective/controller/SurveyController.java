993efde6ab3eed48280befba208c291e3b9d8c5b
zzero-o <2216132@donga.ac.kr>
2024-07-04 06:22:04 +0900

    Feat: SurveyController 설문조사 api 섹션 별 구분

--- src/main/java/aws/retrospective/controller/SurveyController.java
+++ src/main/java/aws/retrospective/controller/SurveyController.java
@@ -13,9 +13,9 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -43,6 +43,36 @@ public class SurveyController {
         List<SurveyDto> surveys = surveyService.getAllSurveys();
 
         // 정상적으로 데이터를 조회한 경우 successResponse 메서드로 응답을 구성
+        return CommonApiResponse.successResponse(HttpStatus.OK, surveys);
+    }
+
+    @Operation(summary = "성별 및 연령 조회", description = "설문조사에서 성별 및 연령 데이터를 조회하는 API")
+    @ApiResponse(responseCode = "200", description = "성별 및 연령 데이터가 성공적으로 조회됨")
+    @GetMapping("/basicInfo")
+    public CommonApiResponse<List<SurveyDto>> getGenderAndAgeSurveys() {
+        List<SurveyDto> surveys = surveyService.getGenderAndAgeSurveys();
+
+        // 정상적으로 데이터를 조회한 경우 successResponse 메서드로 응답 구성
+        return CommonApiResponse.successResponse(HttpStatus.OK, surveys);
+    }
+
+    @Operation(summary = "직업 및 지역 조회", description = "설문조사에서 직업 및 지역 데이터를 조회하는 API")
+    @ApiResponse(responseCode = "200", description = "직업 및 지역 데이터가 성공적으로 조회됨")
+    @GetMapping("/workingInfo")
+    public CommonApiResponse<List<SurveyDto>> getOccupationAndRegionSurveys() {
+        List<SurveyDto> surveys = surveyService.getOccupationAndRegionSurveys();
+
+        // 정상적으로 데이터를 조회한 경우 successResponse 메서드로 응답 구성
+        return CommonApiResponse.successResponse(HttpStatus.OK, surveys);
+    }
+
+    @Operation(summary = "서비스를 알게된 경로, 서비스 사용 목적, 이메일 수신 여부 조회", description = "설문조사에서 서비스 관련 데이터를 조회하는 API")
+    @ApiResponse(responseCode = "200", description = "서비스 관련 데이터가 성공적으로 조회됨")
+    @GetMapping("/serviceInfo")
+    public CommonApiResponse<List<SurveyDto>> getGetSourceAndPurposeSurveys() {
+        List<SurveyDto> surveys = surveyService.getSourceAndPurposeSurveys();
+
+        // 정상적으로 데이터를 조회한 경우 successResponse 메서드로 응답 구성
         return CommonApiResponse.successResponse(HttpStatus.OK, surveys);
     }
 }
