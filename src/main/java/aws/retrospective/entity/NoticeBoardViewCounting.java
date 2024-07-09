b67298c79c7af83e343a3a984bcefb0575bcb169
백민정 <chungchung3701@gmail.com>
2024-07-09 19:49:48 +0900

    feat : NoticeBoardViewCounting

---
+++ src/main/java/aws/retrospective/entity/NoticeBoardViewCounting.java
@@ -0,0 +1,30 @@
+package aws.retrospective.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@RedisHash("NoticeBoardViewCounting")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class NoticeBoardViewCounting {
+
+    @Id
+    private Long postId; // key
+    private Integer viewCount; // value
+
+    public static NoticeBoardViewCounting of(Long postId, Integer viewCount) {
+        return new NoticeBoardViewCounting(postId, viewCount);
+    }
+
+    public void incrementViewCount() {
+        this.viewCount++;
+    }
+
+    public Integer getViewCount() {
+        return this.viewCount;
+    }
+}
+
