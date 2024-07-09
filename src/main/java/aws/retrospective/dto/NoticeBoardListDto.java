b2fb97ec7d3091ce46fd7ef20210053f4ba33d66
백민정 <chungchung3701@gmail.com>
2024-07-09 21:10:08 +0900

    fix : NoticeBoardListDto

--- src/main/java/aws/retrospective/dto/NoticeBoardListDto.java
+++ src/main/java/aws/retrospective/dto/NoticeBoardListDto.java
@@ -3,21 +3,39 @@ package aws.retrospective.dto;
 import aws.retrospective.entity.NoticeBoardWriting;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class NoticeBoardListDto {
     private Long id;
     private String title;
+    private String content;
+    private String status;
     private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
     private int views;
 
     public NoticeBoardListDto(NoticeBoardWriting entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.status = entity.getStatus().name();
         this.createdDate = entity.getCreatedDate();
-        this.views = entity.getViews();
+        this.modifiedDate = entity.getModifiedDate();
+        this.views = 0; // 기본 조회수 초기화
+    }
+
+    public NoticeBoardListDto(NoticeBoardWriting entity, int views) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.status = entity.getStatus().name();
+        this.createdDate = entity.getCreatedDate();
+        this.modifiedDate = entity.getModifiedDate();
+        this.views = views;
     }
 }
