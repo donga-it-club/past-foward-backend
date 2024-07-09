7cbfb23341fe685b315482b17c8a19c7a2a2a782
백민정 <chungchung3701@gmail.com>
2024-07-09 20:06:46 +0900

    fix : NoticeBoardWritingRepository

--- src/main/java/aws/retrospective/repository/NoticeBoardWritingRepository.java
+++ src/main/java/aws/retrospective/repository/NoticeBoardWritingRepository.java
@@ -1,13 +1,10 @@
 package aws.retrospective.repository;
 
 import aws.retrospective.entity.NoticeBoardWriting;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface NoticeBoardWritingRepository extends JpaRepository<NoticeBoardWriting, Long> {
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<NoticeBoardWriting> findById(Long id);
 }
