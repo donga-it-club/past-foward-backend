686584852cacffb6c54a813930f4544628c04382
정민석 <ms.jung.dev@gmail.com>
2024-07-10 19:14:30 +0900

    feat: add trx annotation

--- src/main/java/aws/retrospective/common/CustomAuthenticationConverter.java
+++ src/main/java/aws/retrospective/common/CustomAuthenticationConverter.java
@@ -4,7 +4,6 @@ import aws.retrospective.entity.User;
 import aws.retrospective.repository.UserRepository;
 import java.util.Collections;
 import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
@@ -12,6 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 public class CustomAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
@@ -30,25 +30,26 @@ public class CustomAuthenticationConverter implements Converter<Jwt, AbstractAut
 
         // 사용자가 관리자이면 ROLE_ADMIN 권한을 부여합니다.
         List<GrantedAuthority> authorities = user.isAdministrator() ?
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN")) :
-                Collections.emptyList();
+            Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN")) :
+            Collections.emptyList();
 
         return new UsernamePasswordAuthenticationToken(user, null, authorities);
     }
 
 
-    private User getOrInsertUser(String tenantId, String email, String username) {
+    @Transactional
+    public User getOrInsertUser(String tenantId, String email, String username) {
         return userRepository.findByTenantId(tenantId)
-                .orElseGet(() -> insertUser(tenantId, email, username));
+            .orElseGet(() -> insertUser(tenantId, email, username));
 
     }
 
     private User insertUser(String tenantId, String email, String username) {
         User user = User.builder()
-                .tenantId(tenantId)
-                .email(email)
-                .username(username)
-                .build();
+            .tenantId(tenantId)
+            .email(email)
+            .username(username)
+            .build();
 
         return userRepository.save(user);
     }
