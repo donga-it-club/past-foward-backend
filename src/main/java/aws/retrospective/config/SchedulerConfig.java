d70bb92ebb853cf886ce87a6d47e1fc72970e299
w1shope <wishhopelee@gmail.com>
2024-07-11 14:59:30 +0900

    chore: 테스트 코드에서는 스케줄러 Lock 비활성화

--- src/main/java/aws/retrospective/config/SchedulerConfig.java
+++ src/main/java/aws/retrospective/config/SchedulerConfig.java
@@ -6,10 +6,12 @@ import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
+@Profile("!test") // test 코드에서는 lock 비활성화
 public class SchedulerConfig {
 
     @Bean
