bcbd89a668cdac614c117a8521de93f9ce34913a
w1shope <wishhopelee@gmail.com>
2024-07-13 11:27:00 +0900

    fix: 응답 dto에 comments가 정상적으로 조회되도록 수정

--- src/main/java/aws/retrospective/repository/SectionRepositoryCustomImpl.java
+++ src/main/java/aws/retrospective/repository/SectionRepositoryCustomImpl.java
@@ -48,14 +48,14 @@ public class SectionRepositoryCustomImpl implements SectionRepositoryCustom {
 
     private static Map<Long, List<GetCommentDto>> createCommentMap(List<GetCommentDto> comments) {
         return comments.stream()
-            .collect(Collectors.groupingBy(GetCommentDto::getCommentId));
+            .collect(Collectors.groupingBy(GetCommentDto::getSectionId));
     }
 
     private List<GetCommentDto> getComments(List<Long> sectionIds) {
         return queryFactory
             .select(Projections.constructor(GetCommentDto.class,
-                comment.id, comment.user.id, comment.content, comment.user.username,
-                comment.user.thumbnail))
+                comment.section.id, comment.id, comment.user.id, comment.content, comment.user.username,
+                comment.user.thumbnail, comment.createdDate, comment.updatedDate))
             .from(comment)
             .join(comment.user, user)
             .where(comment.id.in(sectionIds))
