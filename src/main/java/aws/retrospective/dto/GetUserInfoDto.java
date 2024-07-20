5b5272f9a5167d5a5776500407223b2cc57ad1eb
백민정 <chungchung3701@gmail.com>
2024-07-21 01:47:26 +0900

    fix : response 값으로 isAdministrator 요소 추가

--- src/main/java/aws/retrospective/dto/GetUserInfoDto.java
+++ src/main/java/aws/retrospective/dto/GetUserInfoDto.java
@@ -4,8 +4,10 @@ package aws.retrospective.dto;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class GetUserInfoDto {
 
@@ -16,5 +18,5 @@ public class GetUserInfoDto {
     private String phone;
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
-
+    private boolean isAdministrator; // 관리자 여부 추가
 }
